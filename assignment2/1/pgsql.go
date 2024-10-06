8ef4416c16a43d4f6291ee7c973bb92b46cd672d
Gebeko <99469894+Gebeko@users.noreply.github.com>
2024-10-06 17:58:40 +0500

    assignment2

--- assignment2/1/pgsql.go
+++ assignment2/1/pgsql.go
@@ -8,6 +8,54 @@ import (
 	_ "github.com/lib/pq"
 )
 
+func createTable(db *sql.DB) {
+	query := `CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        age INT
+    );`
+
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Table created")
+}
+
+func insertNewUser(db *sql.DB, name string, age int) {
+	query := `INSERT INTO users (name, age) VALUES ($1, $2)`
+	_, err := db.Exec(query, name, age)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("User %s inserted successfully\n", name)
+}
+
+func displayAllUsers(db *sql.DB) {
+	rows, err := db.Query("SELECT id, name, age FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		var age int
+		err := rows.Scan(&id, &name, &age)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("ID: %d, Name: %s, Age: %d\n", id, name, age)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	const (
 		host     = "localhost"
@@ -31,4 +79,12 @@ func main() {
 	}
 
 	fmt.Println("Successfully connected!")
+
+	// Gebe's create table function call
+	createTable(db)
+	// Add user to table
+	insertNewUser(db, "GEBEK", 22)
+	insertNewUser(db, "Baatar", 23)
+	// Query all users
+	displayAllUsers(db)
 }
---
+++ assignment2/2/go.mod
@@ -0,0 +1,3 @@
+module github.com/Gebeko/gorm
+
+go 1.23.1
